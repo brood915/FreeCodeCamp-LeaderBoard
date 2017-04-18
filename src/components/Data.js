bca082cb05bdbbcd362d0032953bd03217cb9c2a
k <brood915@gmail.com>
2017-04-18 11:59:48 -0400

    changed to stateless component

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Data extends React.Component {
-  render() {
+function Data(props) {
     let count = 0;
     return (<table>
       <tbody>
         <tr>
           <th className="left">#</th>
           <th className="left">Name</th>
-          <th value = 'recent' className = {this.props.type === 'recent' ? 'activated' : ''} onClick = {this.props.activate}>Points in the Last 30 Days</th>
-          <th value = "alltime" className = {this.props.type === 'alltime' ? 'activated' : ''} onClick = {this.props.activate}>Total Points</th>
+          <th value = 'recent' className = {props.type === 'recent' ? 'activated' : ''} onClick = {props.activate}>Points in the Last 30 Days</th>
+          <th value = "alltime" className = {props.type === 'alltime' ? 'activated' : ''} onClick = {props.activate}>Total Points</th>
         </tr>
-        {this.props.users.map(function(each) 
+        {props.users.map(function(each) 
        { count++; return (<tr key = {count.toString()}>
         <td className="left">{count}</td>
                                 <td className="left"><a target="_blank" href={'https://www.freecodecamp.com/' + each.username}><img src={each.img}/><span>{each.username}</span></a></td><td>{each.recent}</td>
                                 <td>{each.alltime}</td>
        </tr>);})}
-      </tbody></table>);
-  }
+       </tbody>
+      </table>);
 }
 
 Data.propTypes = {
